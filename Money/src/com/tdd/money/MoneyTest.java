d752b8d12f6b1cd3cc78efea4e0994e1fa3dd60a
Haris Ahmed <Haris Ahmed@HarisAhmed-PC>
2015-01-01 13:14:33 +0500

    Chapter 1. Multi-Currency Money

---
+++ Money/src/com/tdd/money/MoneyTest.java
@@ -0,0 +1,15 @@
+package com.tdd.money;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoneyTest {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+}
